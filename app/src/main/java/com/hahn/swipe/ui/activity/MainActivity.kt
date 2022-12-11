dab974159793259b3ebe3fa4f864be64a182efc7
andrehahn05 <andrehahn05@gmail.com>
2022-12-11 18:48:53 -0400

    xml

--- app/src/main/java/com/hahn/swipe/ui/activity/MainActivity.kt
+++ app/src/main/java/com/hahn/swipe/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.hahn.swipe
+package com.hahn.swipe.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hahn.swipe.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState : Bundle?) {
