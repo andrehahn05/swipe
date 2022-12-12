b0e72b50dbfa282c1ddc9bea83533eb125766276
andrehahn05 <andrehahn05@gmail.com>
2022-12-11 22:29:19 -0400

    refactored

--- app/src/main/java/com/hahn/swipe/ui/activity/MainActivity.kt
+++ app/src/main/java/com/hahn/swipe/ui/activity/MainActivity.kt
@@ -2,19 +2,56 @@ package com.hahn.swipe.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.hahn.swipe.databinding.ActivityMainBinding
+import com.hahn.swipe.model.DataList
 import com.hahn.swipe.ui.recyclerview.adapter.RecyclerAdapter
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var rvAdapder = RecyclerAdapter
+
     private  val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+    private lateinit var rvAdapter: RecyclerAdapter
+    private lateinit var dataList : List<DataList>
+
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        loadList()
+        configLayoutManage()
+        initAdapter()
 
+    }
 
+    private fun configLayoutManage() {
+        val layoutManager : RecyclerView.LayoutManager = LinearLayoutManager(this)
+        binding.rvList.setLayoutManager(layoutManager)
+        binding.rvList.setHasFixedSize(true)
+    }
+
+    private fun initAdapter() {
+        rvAdapter = RecyclerAdapter(dataList)
+        binding.rvList.adapter = rvAdapter
+    }
+
+    private fun loadList() {
+        dataList = listOf(
+            DataList("Java" , "Exp : 3 anos"),
+            DataList("C#" , "Exp : 7 anos"),
+            DataList("Kotlin" , "Exp : 3 anos"),
+            DataList("Python" , "Exp : 4 anos"),
+            DataList("JavaScript" , "Exp : 6 anos"),
+            DataList("C" , "Exp : 5 anos"),
+            DataList("C++" , "Exp : 5 anos"),
+            DataList("Rust", "Exp: 2 anos"),
+            DataList("Swift", "Exp: 3 anos"),
+            DataList("Perl", "Exp: 2 anos"),
+            DataList("Delphi", "Exp: 2 anos"),
+            DataList("PHP" , "Exp : 1/2 ano"),
+
+        )
     }
 
 }
--- app/src/main/java/com/hahn/swipe/ui/recyclerview/adapter/RecyclerAdapter.kt
+++ app/src/main/java/com/hahn/swipe/ui/recyclerview/adapter/RecyclerAdapter.kt
@@ -1,16 +1,18 @@
 package com.hahn.swipe.ui.recyclerview.adapter
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.hahn.swipe.databinding.ListItemBinding
 import com.hahn.swipe.model.DataList
 
-class RecyclerAdapter(var dataList :  List<DataList>)
-    : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+class RecyclerAdapter(var dataList : List<DataList>) :
+    RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent : ViewGroup , viewType : Int) : ViewHolder {
         val binding = ListItemBinding.inflate(LayoutInflater.from(parent.context) , parent , false)
+        Log.i("VH" , "vh:${binding} ")
         return ViewHolder(binding)
     }
 
@@ -18,7 +20,9 @@ class RecyclerAdapter(var dataList :  List<DataList>)
         holder.bind(dataList.get(position))
     }
 
-    override fun getItemCount() : Int = dataList.size
+    override fun getItemCount() : Int {
+        return dataList.size
+    }
 
     inner class ViewHolder(val binding : ListItemBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(get : DataList) {
